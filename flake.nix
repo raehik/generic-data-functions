ea647ab14f90d2195f02fb948c0210897572af64
Ben Orchard <thefirstmuffinman@gmail.com>
2023-10-18 15:57:21 +0100

    try to build with GHC 9.8
    not working

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs/haskell-updates";
+    nixpkgs.url = "github:vaibhavsagar/nixpkgs/vaibhavsagar/ghc981";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
   };
@@ -11,8 +12,9 @@
       perSystem = { self', pkgs, config, ... }: {
         packages.default = self'.packages.generic-data-functions;
         haskellProjects.ghc96 = import ./haskell-flake-ghc96.nix pkgs;
+        haskellProjects.ghc98 = import ./haskell-flake-ghc98.nix pkgs;
         haskellProjects.default = {
-          #basePackages = config.haskellProjects.ghc96.outputs.finalPackages;
+          basePackages = config.haskellProjects.ghc98.outputs.finalPackages;
           devShell = {
             tools = hp: {
               ghcid = null; # broken on GHC 9.6? fsnotify
