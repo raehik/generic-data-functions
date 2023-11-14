56e54a59179feb33388925d1c8e5c1589fe1f647
Ben Orchard <thefirstmuffinman@gmail.com>
2023-11-14 17:23:29 +0000

    update flake config (GHC 9.6, 9.4; no 9.8)

--- flake.nix
+++ flake.nix
@@ -1,25 +1,23 @@
 {
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/haskell-updates";
-    nixpkgs.url = "github:raehik/nixpkgs/raehik/ghc981";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
       perSystem = { self', pkgs, config, ... }: {
-        packages.default = self'.packages.generic-data-functions;
-        haskellProjects.ghc96 = import ./haskell-flake-ghc96.nix pkgs;
-        haskellProjects.ghc98 = import ./haskell-flake-ghc98.nix pkgs;
-        haskellProjects.default = {
-          basePackages = config.haskellProjects.ghc98.outputs.finalPackages;
-          devShell = {
-            tools = hp: {
-              ghcid = null; # broken on GHC 9.6? fsnotify
-              hlint = null; # broken on GHC 9.6?
-              haskell-language-server = null; # TAKES AGES TO BUILD FFS
+        packages.default = self'.packages.ghc96-generic-data-functions;
+        haskellProjects.ghc94.basePackages = pkgs.haskell.packages.ghc94;
+        haskellProjects.ghc96 = {
+          # 2023-11-14: GHC 9.6 base package set is borked
+          # PR: https://github.com/NixOS/nixpkgs/pull/267477
+          basePackages = pkgs.haskell.packages.ghc96.override {
+            overrides = self: super: {
+              fgl = self.fgl_5_8_2_0;
+              th-desugar = self.th-desugar_1_16;
             };
           };
         };
