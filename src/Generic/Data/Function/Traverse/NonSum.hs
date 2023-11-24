ad47357b3bb535fb46220a6adeeb7d63e338ec4d
Ben Orchard <thefirstmuffinman@gmail.com>
2023-11-24 04:53:11 +0000

    traverse/nonsum: use existing error strings

--- src/Generic/Data/Function/Traverse/NonSum.hs
+++ src/Generic/Data/Function/Traverse/NonSum.hs
@@ -5,15 +5,13 @@ module Generic.Data.Function.Traverse.NonSum where
 
 import GHC.Generics
 import Generic.Data.Function.Traverse.Constructor ( GTraverseC(gTraverseC) )
+import Generic.Data.Rep.Error
 
 class GTraverseNonSum (cd :: Meta) f f' where gTraverseNonSum :: f (f' p)
-
-instance GTraverseNonSum cd f (l :+: r) where
-    gTraverseNonSum = undefined
 
 instance (Functor f, GTraverseC cd cc 0 f f')
   => GTraverseNonSum cd f (C1 cc f') where
     gTraverseNonSum = M1 <$> gTraverseC @cd @cc @0
 
-instance GTraverseNonSum cd f V1 where
-    gTraverseNonSum = undefined
+instance GTraverseNonSum cd f (l :+: r) where gTraverseNonSum = error eNoSum
+instance GTraverseNonSum cd f V1        where gTraverseNonSum = error eNoEmpty
